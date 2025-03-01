7a1abed20d8a5cecb78be6893d7b4b57602f386d
LDThien999 <leepro0611@gmail.com>
2025-03-01 09:48:14 +0700

    firebase config

---
+++ social-media/src/main/java/com/example/social_media/config/FirebaseConfig.java
@@ -0,0 +1,30 @@
+package com.example.social_media.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+    @Bean
+    public Firestore firestore() throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(fileInputStream))
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(firebaseOptions);
+        }
+
+        return FirestoreClient.getFirestore();
+    }
+}
